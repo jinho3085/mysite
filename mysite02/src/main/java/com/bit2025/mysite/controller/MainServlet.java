af020595dd32de19e211166054608c38cf14e2d5
jinho3085 <jinho3085@naver.com>
2025-08-26 10:25:19 +0900

    guestbook

--- mysite02/src/main/java/com/bit2025/mysite/controller/MainServlet.java
+++ mysite02/src/main/java/com/bit2025/mysite/controller/MainServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
