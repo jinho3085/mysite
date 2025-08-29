cf083c813971a1a91af2a825141684fb091ee944
jinho3085 <jinho3085@naver.com>
2025-08-29 09:25:57 +0900

    Board

--- mysite02/src/main/java/com/bit2025/mysite/controller/BoardServlet.java
+++ mysite02/src/main/java/com/bit2025/mysite/controller/BoardServlet.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class BoradServlet extends HttpServlet {
+public class BoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
